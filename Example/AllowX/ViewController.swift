70ea5a7f699b79f3ad3941f98badb1bc76c9b74e
KarimEbrahem <karimabdelazizmansour@gmail.com>
2020-08-03 13:53:54 +0200

    Update example usage for AllowX

--- Example/AllowX/ViewController.swift
+++ Example/AllowX/ViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 import AllowX
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        AllowX.primaryColor = UIColor.systemPink
-        let permission: AllowX = .notifications
-
-        print(permission.status) // .notDetermined
-
+        AllowX.primaryColor = .systemPink
+        
+        let permission: AllowX = AllowX(type: .camera)
+        
+        permission.image = UIImage(named: "camera")
+        permission.title = "Camera Custom Access"
+        permission.message = "Please give us permission to capture your awesome moments."
+        permission.cancelButtonTitle = "Cancel"
+        permission.notNowButtonTitle = "Not now"
+        permission.goToSettingsButtonTitle = "Go to Settings"
+        permission.confirmButtonTitle = "Allow Permission"
+        
+        permission.presentPrePermissionAlert = true
+        permission.presentDeniedAlert = true
+        permission.presentDisabledAlert = true
+        
         permission.request { status in
             switch status {
             case .authorized:    print("authorized")
@@ -28,6 +39,6 @@ class ViewController: UIViewController {
             }
         }
     }
-
+    
 }
 
