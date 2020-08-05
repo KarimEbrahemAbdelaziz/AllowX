ddd753bf9b38fb5fe091631cdc621eb57c763234
KarimEbrahem <karimabdelazizmansour@gmail.com>
2020-08-05 16:30:52 +0200

    Update Example

--- Example/AllowX/ViewController.swift
+++ Example/AllowX/ViewController.swift
@@ -16,28 +16,8 @@ class ViewController: UIViewController {
         
         AllowX.primaryColor = .systemPink
         
-        let permission: AllowX = AllowX(type: .camera)
-        
-        permission.alertHeight = .constant(value: 400)
-        permission.image = UIImage(named: "camera")
-        
-        permission.title = "Camera Custom Access"
-        permission.titleFont = .boldSystemFont(ofSize: 22)
-        permission.titleColor = . darkGray
-        
-        permission.message = "Please give us permission to capture your awesome moments."
-        permission.messageFont = .systemFont(ofSize: 16)
-        permission.messageColor = .gray
-        
-        permission.cancelButtonTitle = "Cancel"
-        permission.notNowButtonTitle = "Not now"
-        permission.goToSettingsButtonTitle = "Go to Settings"
-        permission.confirmButtonTitle = "Allow Permission"
-        permission.buttonTitleFont = .boldSystemFont(ofSize: 14)
-        
-        permission.presentPrePermissionAlert = true
-        permission.presentDeniedAlert = true
-        permission.presentDisabledAlert = true
+        let configuration = CustomCameraConfiguration()
+        let permission: AllowX = AllowX(type: .camera, configuration: configuration)
         
         permission.request { status in
             switch status {
