34bf7eaf7679650aa799d25e35be8329c8c085cc
KarimEbrahem <karimabdelazizmansour@gmail.com>
2020-08-05 16:29:53 +0200

    Change permission alerts to use AllowX Configuration Protocol

--- AllowX/Classes/AllowXAlerts/DisabledAlert/DisabledAlert.swift
+++ AllowX/Classes/AllowXAlerts/DisabledAlert/DisabledAlert.swift
@@ -44,127 +44,95 @@ class DisabledAlert: UIView {
 
     fileprivate var callbacks: AllowX.Callback? { return permission?.callbacks }
 
-    /// The image of the alert.
-    open var image: UIImage? {
-        guard let image = permission?.image else {
-            guard let type = type else {
-                return nil
-            }
-            
-            switch type {
-            case .camera:
-                return permission?.cameraPermissionImage
-            case .locationAlways, .locationWhenInUse:
-                return permission?.locationPermissionImage
-            case .notifications:
-                return permission?.notificationPermissionImage
-            }
-        }
-        
-        return image
-    }
-    
-    /// The title of the alert.
-    open var title: String? {
-        guard let type = type else {
-            return nil
-        }
-        
-        switch type {
-        case .camera:
-            return permission?.title ?? "Camera Access"
-        case .locationAlways, .locationWhenInUse:
-            return permission?.title ?? "Location Access"
-        case .notifications:
-            return permission?.title ?? "Notification Access"
-        }
-    }
-
-    /// Descriptive text that provides more details about the reason for the alert.
-    open var message: String? {
-        guard let type = type else {
-            return nil
-        }
-        
-        switch type {
-        case .camera:
-            return permission?.message ?? "To capture awesome photos of you."
-        case .locationAlways, .locationWhenInUse:
-            return  permission?.message ?? "Please enable location access to use this feature"
-        case .notifications:
-            return  permission?.message ?? "Enable push notifications to let us send you personal news and updates"
-        }
-    }
-
-    /// The title of the cancel action.
-    open var cancel: String? {
-        guard let status = status else {
-            return nil
-        }
-        
-        switch status {
-        case .authorized:
-            return nil
-        case .denied:
-            return permission?.cancelButtonTitle ?? "Cancel"
-        case .disabled:
-            return permission?.cancelButtonTitle ?? "Cancel"
-        case .notDetermined:
-            return permission?.notNowButtonTitle ?? "Not Now"
-        }
-    }
-
-    /// The title of the confirm action.
-    open var confirm: String? {
-        guard let status = status else {
-            return nil
-        }
-        
-        switch status {
-        case .authorized:
-            return nil
-        case .denied:
-            return permission?.goToSettingsButtonTitle ?? "Go to Settings"
-        case .disabled:
-            return permission?.goToSettingsButtonTitle ?? "Go to Settings"
-        case .notDetermined:
-            return permission?.confirmButtonTitle ?? "Allow"
-        }
-    }
-    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        setup()
     }
     
-    init(permission: AllowX) {
+    init(permission: AllowX, configuration: AllowXConfiguration) {
         super.init(frame: .zero)
         
         self.permission = permission
-        setup()
+        setup(configuration: configuration)
     }
     
-    private func setup() {
+    private func setup(configuration: AllowXConfiguration) {
         fromNib()
         
         clipsToBounds = true
         layer.cornerRadius = 16
-        configure()
+        configure(configuration: configuration)
     }
     
-    private func configure() {
-        self.iconImageView.image = image
-        self.titleLabel.text = title
-        self.titleLabel.font = permission?.titleFont
-        self.titleLabel.textColor = permission?.titleColor
-        self.messageLabel.text = message
-        self.messageLabel.font = permission?.messageFont
-        self.messageLabel.textColor = permission?.messageColor
-        self.dismissButton.setTitle(cancel, for: .normal)
-        self.dismissButton.titleLabel?.font = permission?.buttonTitleFont
-        self.doneButton.setTitle(confirm, for: .normal)
-        self.doneButton.titleLabel?.font = permission?.buttonTitleFont
+    private func configure(configuration: AllowXConfiguration) {
+        self.iconImageView.image = configuration.image
+        
+        setupTitleLabel(configuration: configuration)
+        setupMessageLabel(configuration: configuration)
+        setupCancelButton(configuration: configuration)
+        setupConfirmButton(configuration: configuration)
+    }
+    
+    private func setupTitleLabel(configuration: AllowXConfiguration) {
+        guard let status = status else {
+            return
+        }
+        
+        switch status {
+        case .authorized, .notDetermined:
+            self.titleLabel.text = configuration.prePermissionTitle
+        case .denied:
+            self.titleLabel.text = configuration.deniedTitle
+        case .disabled:
+            self.titleLabel.text = configuration.disabledTitle
+        }
+        self.titleLabel.font = configuration.titleFont
+        self.titleLabel.textColor = configuration.titleColor
+    }
+    
+    private func setupMessageLabel(configuration: AllowXConfiguration) {
+        guard let status = status else {
+            return
+        }
+        
+        switch status {
+        case .authorized, .notDetermined:
+            self.messageLabel.text = configuration.prePermissionMessage
+        case .denied:
+            self.messageLabel.text = configuration.deniedMessage
+        case .disabled:
+            self.messageLabel.text = configuration.disabledMessage
+        }
+        self.messageLabel.font = configuration.messageFont
+        self.messageLabel.textColor = configuration.messageColor
+    }
+    
+    private func setupCancelButton(configuration: AllowXConfiguration) {
+        guard let status = status else {
+            return
+        }
+        
+        switch status {
+        case .authorized, .denied, .disabled:
+            self.dismissButton.setTitle(configuration.cancelButtonTitle, for: .normal)
+        case .notDetermined:
+            self.dismissButton.setTitle(configuration.notNowButtonTitle, for: .normal)
+        }
+        self.dismissButton.titleLabel?.font = configuration.buttonTitleFont
+    }
+    
+    private func setupConfirmButton(configuration: AllowXConfiguration) {
+        guard let status = status else {
+            return
+        }
+        
+        switch status {
+        case .authorized, .notDetermined:
+            self.doneButton.setTitle(configuration.confirmButtonTitle, for: .normal)
+        case .denied, .disabled:
+            self.doneButton.setTitle(configuration.goToSettingsButtonTitle, for: .normal)
+        }
+        self.doneButton.titleLabel?.font = configuration.buttonTitleFont
     }
     
     @IBAction private func dismissButtonTapped(_ sender: Any) {
